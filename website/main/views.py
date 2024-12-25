0e9697579c89e0d60d6e8f14361d259ff7af3df5
mynkchn <mynkchn.pu@gmail.com>
2024-12-25 11:33:06 +0530

    Add files via upload

---
+++ website/main/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout
+# Create your views here.
+def home(request) :
+   return render(request,'auth/home.html',{})
+
+
+
+def register_view(request) :
+    if request.method=='POST' :
+      
+      form=UserCreationForm(request.POST) 
+      if form.is_valid() :
+            print("Form is valid. Saving user...")
+            user=form.save()
+            return redirect('/login')
+        # Debugging line to log form errors
+    else :
+     form=UserCreationForm()
+  
+    return render(request,'auth/register.html',{'form':form})
+
+def login_view(request):
+     if request.method=='POST' :
+        form=AuthenticationForm(data=request.POST)
+        if form.is_valid() : 
+           user=form.get_user()
+           login(request,user)
+           return redirect('/home')
+     else :
+        form=AuthenticationForm()
+     return render(request,'auth/login.html',{'form':form})     
+
+def dashboard(request) :
+   return render(request,'auth/dashboard.html',{})       
+
+def logout(request) :
+   return redirect('/home/')
